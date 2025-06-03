e611c5c242e85ed9c6c9e3f6be1c271ce0e49be1
sakshi16-b <sakshibisht016@gmail.com>
2025-06-03 21:03:35 +0530

    convert-ts-file to js-file

---
+++ app.js
@@ -0,0 +1,6 @@
+var userName = "sakshi bisht";
+var age = 30;
+var college = "GEU Dehradun";
+console.log(userName);
+console.log(age);
+console.log(college);
---
+++ app.ts
@@ -0,0 +1,6 @@
+var userName:string="sakshi bisht"
+var age:Number=30
+var college:string="GEU Dehradun"
+console.log(userName)
+console.log(age)
+console.log(college)
