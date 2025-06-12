94791c104ec74d9d2d1dbaa68de180c64efae9a8
sakshi16-b <sakshibisht016@gmail.com>
2025-06-12 07:47:42 +0530

    done

---
+++ array.ts
@@ -0,0 +1,12 @@
+var users:string[]=['sakshi','Bruce','Peter']
+var marks:number[]=[ 20,40.50,100]
+var students:Array<string>=['Rahul','Nikhil','Raj']
+students.push("Megha,Sakshi");
+students.pop();
+console.log(students)
+marks.push(90)
+console.log(marks)
+var collegeNames:ReadonlyArray<string>=['IIT','GEU','KIT','UIT']
+console.log(collegeNames)
+//ReadonlyArray can't be manipulate the array
+
